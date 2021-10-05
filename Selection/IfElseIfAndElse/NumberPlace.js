b791291905ba2c3733d86eba7fc4970ad93bd8b0
varshini <dreamvarshini@gmail.com>
2021-10-05 22:08:45 +0530

    ADDED: printing number value

---
+++ Selection/IfElseIfAndElse/NumberPlace.js
@@ -0,0 +1,22 @@
+let number=10;
+var numberValue;
+if(number==1)
+{
+    numberValue="unit"
+}
+else if(number==10)
+{
+    numberValue="ten"
+
+}
+else if(number==100)
+{
+    numberValue="hundred"
+
+}
+else if(number==1000)
+{
+    numberValue="thousand"
+
+}
+console.log(numberValue)
