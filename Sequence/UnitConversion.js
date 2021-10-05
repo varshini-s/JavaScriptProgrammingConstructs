d6cbc03d9d51ccde15f181fce6f93414ad487d91
varshini <dreamvarshini@gmail.com>
2021-10-05 20:26:35 +0530

    MODIFIED: finding area of given plot

--- Sequence/UnitConversion.js
+++ Sequence/UnitConversion.js
@@ -6,3 +6,16 @@ var convert = require('convert-units')
     console.log("inches: " + inch + " in feet is " + convertedValue)
 }
 
+{
+
+    let length=60;
+    let width=40;
+    let lengthInMeters=convert(length).from('ft').to('m');
+    let widthInMeters=convert(width).from('ft').to('m');
+    console.log("plot value in meters is"+lengthInMeters+"X"+widthInMeters);
+    let numberOfPlots=25;
+    let areaInSquareFeet=length*width*numberOfPlots;
+    let areaInAcre=convert(areaInSquareFeet).from('ft2').to('ac');
+    console.log("Area of "+numberOfPlots+" plots in acres is:"+areaInAcre);
+
+}
