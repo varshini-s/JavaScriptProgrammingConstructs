b1ab0123e7ce7b2f94b6bbca9cc0c4ce6378ab4f
varshini <dreamvarshini@gmail.com>
2021-10-05 22:05:07 +0530

    ADDED: reading nnumber and printing week day

---
+++ Selection/IfElseIfAndElse/WeekDay.js
@@ -0,0 +1,38 @@
+var day;
+let dayNumber=2;
+if (dayNumber == 0) 
+{
+    day = "Sunday";
+}
+else if (dayNumber == 1) 
+{
+    day = "Monday";
+
+}
+else if (dayNumber == 2) 
+{
+    day = "Tuesday";
+
+}
+else if (dayNumber == 3) 
+{
+    day = "Wednesday";
+
+}
+
+else if (dayNumber == 4) 
+{
+    day = "Thursday";
+
+}
+else if (dayNumber == 5)
+ {
+    day = "Friday";
+
+}
+else if (dayNumber == 6) 
+{
+    day = "Saturday";
+
+}
+console.log(day)
