e94e230939f085f29973e600bb4832a64db3d9c4
varshini <dreamvarshini@gmail.com>
2021-10-06 14:25:48 +0530

    REFACTORED: mapping same birth month between year 92 and 93

--- Map/SameBirthMonth.js
+++ Map/SameBirthMonth.js
@@ -1,20 +1,38 @@
-let birthMonthMap = new Map();
-for(let index=1;index<=50;index++)
+let birthMonthMap,birthYear;
+let birthYearMap = new Map();
+
+birthYearMap.set(1992,new Map())
+birthYearMap.set(1993,new Map())
+ 
+for (let index = 1; index <= 50; index++) 
 {
-    let person=index;
-    let birthMonth=(Math.floor(Math.random()*100)%12)+1;
-    if(birthMonthMap.has(birthMonth))
+    let getYear = Math.floor(Math.random() * 10) % 2;
+
+    if(getYear==0)
     {
-        let peopleWithSameBirthMonthArray=birthMonthMap.get(birthMonth);
-        peopleWithSameBirthMonthArray.push(person);
-         birthMonthMap.set(birthMonth,peopleWithSameBirthMonthArray);
+        birthYear=1992;
     }
-    else
+    else if(getYear==1)
     {
-        let peopleWithSameBirthMonthArray=new Array();
-        peopleWithSameBirthMonthArray.push(person);
-        birthMonthMap.set(birthMonth,peopleWithSameBirthMonthArray);
+        birthYear=1993;  
     }
+
+    birthMonthMap=birthYearMap.get(birthYear)
+    let person = index;
+    let birthMonth = (Math.floor(Math.random() * 100) % 12) + 1;
+    if (birthMonthMap.has(birthMonth)) 
+    {
+        let peopleWithSameBirthMonthArray = birthMonthMap.get(birthMonth);
+        peopleWithSameBirthMonthArray.push(person);
+        birthMonthMap.set(birthMonth, peopleWithSameBirthMonthArray);
+    }
+    else 
+    {
+        let peopleWithSameBirthMonthArray = new Array();
+        peopleWithSameBirthMonthArray.push(person);
+        birthMonthMap.set(birthMonth, peopleWithSameBirthMonthArray);
+    }
+    birthYearMap.set(birthYear, birthMonthMap)
 }
 console.log("People with same birth month ")
-console.log(birthMonthMap)
+console.log(birthYearMap)
